8288a47f055eeaa9c70d5c687489750b86246607
KubatAbyshov <flimdeweack@gmail.com>
2019-10-13 20:16:43 +0600

    Исправлено

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import java.awt.List;
 import java.util.*;
 
 public class Main {
@@ -8,13 +7,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         ArrayList<String> listA = new ArrayList<>(5);
         while (listA.size() < 5) {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Введите имя для списка А: ");
             String s = scanner.nextLine();
             listA.add(s);
+            listA.toArray();
         }
         System.out.println("Имя: " + listA);
 
@@ -27,42 +26,58 @@ public class Main {
             System.out.println("Введите имя для списка Б: ");
             String s = scanner.nextLine();
             listB.add(s);
+            listB.toArray();
         }
+        Collections.reverse(listB);
         System.out.println("Имя: " + listB);
 
         System.out.println("___________________________");
 
         ArrayList<String> listC = new ArrayList<>(10);
-        listC.add(listA.get(0));
-        listC.add(listB.get(4));
-        listC.add(listA.get(1));
-        listC.add(listB.get(3));
-        listC.add(listA.get(2));
-        listC.add(listB.get(2));
-        listC.add(listA.get(3));
-        listC.add(listB.get(1));
-        listC.add(listA.get(4));
-        listC.add(listB.get(0));
 
+        Iterator i1 = listA.iterator();
+        Iterator i2 = listB.iterator();
+
+        while (listC.size() < 10) {
+
+            listC.add((String) i1.next());
+            listC.add((String) i2.next());
+        }
         System.out.println(listC);
-
         System.out.println("___________________________");
 
 
         Collections.sort(listC, new Comparator<String>() {
-            @Override
+
             public int compare(String x1, String x2) {
                 return x1.length() - x2.length();
             }
         });
-
-        for (String s : listC) {
-            System.out.println(s);
-
-        }
-
+        System.out.println(listC);
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
